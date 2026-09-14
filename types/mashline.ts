8ab6911e4bab7b7c2a3894fe22ee0e2e62cf9c55
Rikansa. <147376009+Rikansaa@users.noreply.github.com>
2026-09-15 04:56:09 +0700

    Update mashline.ts

--- types/mashline.ts
+++ types/mashline.ts
@@ -1,14 +1,7 @@
-declare module "meshline" {
-  export const MeshLineGeometry: any;
-  export const MeshLineMaterial: any;
-}
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      meshLineGeometry: any;
-      meshLineMaterial: any;
-    }
+declare module "@react-three/fiber" {
+  interface ThreeElements {
+    meshLineGeometry: any;
+    meshLineMaterial: any;
   }
 }
 
