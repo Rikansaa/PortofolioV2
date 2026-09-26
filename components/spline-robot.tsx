ecb099b5293a23632c99309f30134734a09d444e
Rikansa. <147376009+Rikansaa@users.noreply.github.com>
2026-09-26 13:10:53 +0700

    Update spline-robot.tsx

--- components/spline-robot.tsx
+++ components/spline-robot.tsx
@@ -4,7 +4,7 @@ export default function SplineRobot() {
   return (
     <div className="spline-robot-wrapper h-full w-full">
       <Spline
-        scene="https://prod.spline.design/CGDWpKgcUaDjp8BX/scene.splinecode"
+        scene="https://prod.spline.design/kNv8LxltNsZN1PKF/scene.splinecode"
         style={{ width: "100%", height: "100%", background: "transparent" }}
       />
     </div>
