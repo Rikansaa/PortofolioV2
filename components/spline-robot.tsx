9f18a559f169199bbeded07f134aee94ac8aa8dd
Rikansa. <147376009+Rikansaa@users.noreply.github.com>
2026-09-26 13:17:41 +0700

    Update spline-robot.tsx

--- components/spline-robot.tsx
+++ components/spline-robot.tsx
@@ -2,7 +2,7 @@ import Spline from "@splinetool/react-spline/next";
 
 export default function SplineRobot() {
   return (
-    <div className="spline-robot-wrapper h-full w-full">
+    <div className="spline-robot-wrapper pointer-events-none fixed inset-0 z-20 flex h-full w-full items-center justify-center">
       <Spline
         scene="https://prod.spline.design/czzuNfs3tQxeJplz/scene.splinecode"
       />
